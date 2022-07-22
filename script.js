386063d242e956bacfedc20f4251bdda4b927577
marty-zhu <marty.zhu@gmail.com>
2022-07-21 17:29:18 -0700

    added evenlistener to individual grid pixels

--- script.js
+++ script.js
@@ -3,8 +3,11 @@ const container = document.querySelector('div.container');
 for (let n = 0; n < 256; n++) {
     const grid = document.createElement('div');
     grid.classList.add('pixel');
-    grid.classList.add('inactive');
     grid.textContent = n + 1;
+
+    grid.addEventListener('mouseover', () => {
+        grid.classList.add('active');
+    })
 
     container.appendChild(grid);
 }
