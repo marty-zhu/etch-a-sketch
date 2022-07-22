6be4d4af3af10793245ea5e17a949ee5b0ea45c2
marty-zhu <marty.zhu@gmail.com>
2022-07-21 20:31:19 -0700

    limited max canvas width to 100

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 const container = document.querySelector('div.container');
 const rootCanvasWidth = document.querySelector(':root');
 
-let sideWidth = prompt("What size would you like for the canvas? (width only)");
-let totalNumPixel = parseInt(sideWidth)**2;
+let sideWidthString = prompt("What size would you like for the canvas? (width only)");
+let sideWidth = parseInt(sideWidthString);
+
+if (sideWidth > 100) { sideWidth = 100; };
+let totalNumPixel = sideWidth**2;
 
 rootCanvasWidth.style.setProperty('--canvas-side-width', sideWidth);
 
