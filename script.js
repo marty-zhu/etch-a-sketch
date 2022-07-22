e61246cfea6e5aaa5cc9f900b689faf0e7cfbf99
marty-zhu <marty.zhu@gmail.com>
2022-07-22 10:43:53 -0700

    added 90% view height calculation to script

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeig
 function setCanvas() {
     let sideWidthString = prompt("What size would you like for the canvas? (width only)");
     let sideWidth = parseInt(sideWidthString);
-    let pixelWidth = Math.max(vh / sideWidth, 1);
+    let pixelWidth = Math.max(vh * 0.9 / sideWidth, 1);
 
     if (sideWidth > 100) { sideWidth = 100; };
     let totalNumPixel = sideWidth**2;
@@ -19,6 +19,7 @@ function setCanvas() {
     for (let n = 0; n < totalNumPixel; n++) {
         const grid = document.createElement('div');
         grid.classList.add('pixel');
+        grid.classList.add('border');
 
         grid.addEventListener('mouseover', () => {
             grid.classList.add('active');
