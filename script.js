baba5312bc5ba6a1b65fba14e1909a369dbb5d08
marty-zhu <marty.zhu@gmail.com>
2022-07-22 07:15:23 -0700

    split js code to setup and teardown canvas

--- script.js
+++ script.js
@@ -1,26 +1,37 @@
 const container = document.querySelector('div.container');
 const rootCanvasWidth = document.querySelector(':root');
 
-let sideWidthString = prompt("What size would you like for the canvas? (width only)");
-let sideWidth = parseInt(sideWidthString);
+// TODO: figure out why this is not working
+function setCanvas() {
+    let sideWidthString = prompt("What size would you like for the canvas? (width only)");
+    let sideWidth = parseInt(sideWidthString);
 
-if (sideWidth > 100) { sideWidth = 100; };
-let totalNumPixel = sideWidth**2;
+    if (sideWidth > 100) { sideWidth = 100; };
+    let totalNumPixel = sideWidth**2;
 
-rootCanvasWidth.style.setProperty('--canvas-side-width', sideWidth);
+    rootCanvasWidth.style.setProperty('--canvas-side-width', sideWidth);
 
-const canvas = document.createElement('div');
-canvas.classList.add('canvas');
+    const canvas = document.createElement('div');
+    canvas.classList.add('canvas');
 
-for (let n = 0; n < totalNumPixel; n++) {
-    const grid = document.createElement('div');
-    grid.classList.add('pixel');
+    for (let n = 0; n < totalNumPixel; n++) {
+        const grid = document.createElement('div');
+        grid.classList.add('pixel');
 
-    grid.addEventListener('mouseover', () => {
-        grid.classList.add('active');
-    })
+        grid.addEventListener('mouseover', () => {
+            grid.classList.add('active');
+        })
 
-    canvas.appendChild(grid);
+        canvas.appendChild(grid);
+    };
+
+    container.appendChild(canvas);
 };
 
-container.appendChild(canvas);
+function resetCanvas() {
+    const canvas = document.querySelector('div.canvas');
+    container.removeChild(canvas);
+    setCanvas();
+}
+
+setCanvas();
