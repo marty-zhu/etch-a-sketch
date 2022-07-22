21ded9deb7c80d63831b9bc19e6ca7f656328c52
marty-zhu <marty.zhu@gmail.com>
2022-07-22 10:14:21 -0700

    canvas now fixed size with flexible pixels

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 const container = document.querySelector('div.container');
 const rootCanvasWidth = document.querySelector(':root');
+const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
 
 function setCanvas() {
     let sideWidthString = prompt("What size would you like for the canvas? (width only)");
     let sideWidth = parseInt(sideWidthString);
+    let pixelWidth = Math.max(vh / sideWidth, 1);
 
     if (sideWidth > 100) { sideWidth = 100; };
     let totalNumPixel = sideWidth**2;
 
     rootCanvasWidth.style.setProperty('--canvas-side-width', sideWidth);
+    rootCanvasWidth.style.setProperty('--pixel-width', pixelWidth);
 
     const canvas = document.createElement('div');
     canvas.classList.add('canvas');
