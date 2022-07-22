6da0e633b8fe9c46317a044a5d4501b0275a51b3
marty-zhu <marty.zhu@gmail.com>
2022-07-21 17:04:29 -0700

    added script to add 16x16 grid via js

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 const container = document.querySelector('div.container');
 
+for (let n = 0; n < 256; n++) {
+    const grid = document.createElement('div');
+    grid.classList.add('pixel');
+    grid.classList.add('inactive');
+    grid.textContent = n + 1;
+
+    container.appendChild(grid);
+}
