5b6d0491c030b06d41d1b046d88c05aab751fe65
marty-zhu <marty.zhu@gmail.com>
2022-07-21 20:18:43 -0700

    added prompt to ask user for canvas size

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 const container = document.querySelector('div.container');
 
-for (let n = 0; n < 256; n++) {
+let sideWidth = prompt("What size would you like for the canvas? (width only)");
+let totalNumPixel = parseInt(sideWidth)**2;
+
+for (let n = 0; n < totalNumPixel; n++) {
     const grid = document.createElement('div');
     grid.classList.add('pixel');
-    grid.textContent = n + 1;
+    // grid.textContent = n + 1;
 
     grid.addEventListener('mouseover', () => {
         grid.classList.add('active');
